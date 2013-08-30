d86f49fbe507940e1101778efe9c132dff3be654
Chris Lansing <cslansin@gmail.com>
2013-08-30 15:32:09 -0400

    updated structure, added settings and base template, dev and prod setup

--- dev_manage.py
+++ dev_manage.py
@@ -5,7 +5,7 @@ import sys
 sys.path.append('/data/Django-1.5.2')
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cslansing.settings.default")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cslansing.settings.dev")
 
     from django.core.management import execute_from_command_line
 
