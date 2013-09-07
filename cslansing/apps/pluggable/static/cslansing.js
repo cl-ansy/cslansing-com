118a61548b479bbbf639636a5d6a37665e510ad4
Chris Lansing <cslansin@gmail.com>
2013-09-07 14:26:32 -0400

    base template style, css, js changes

---
+++ cslansing/apps/pluggable/static/cslansing.js
@@ -0,0 +1,12 @@
+$('#project-one').click(function() {
+  $('#project-one-content').slideToggle();
+  $("span",this).toggleClass("glyphicon-chevron-down glyphicon-chevron-up");
+});
+$('#project-two').click(function() {
+  $('#project-two-content').slideToggle();
+  $("span",this).toggleClass("glyphicon-chevron-down glyphicon-chevron-up");
+});
+$('#project-three').click(function() {
+  $('#project-three-content').slideToggle();
+  $("span",this).toggleClass("glyphicon-chevron-down glyphicon-chevron-up");
+});
