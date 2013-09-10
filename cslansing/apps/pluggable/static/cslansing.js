68c18c7b8d7eff9693e5c08138c01adabe737276
Chris Lansing <cslansin@gmail.com>
2013-09-10 10:39:30 -0400

    added social links in base template; removed sidenav icon change js

--- cslansing/apps/pluggable/static/cslansing.js
+++ cslansing/apps/pluggable/static/cslansing.js
@@ -1,14 +1,10 @@
-$('#sidenav-button').click(function() {
+/*$('#sidenav-button').click(function() {
   $("i",this).toggleClass("icon-double-angle-down icon-double-angle-up");
-});
+});*/
 $('.section-header').click(function() {
     $(this).next('.section-content').slideToggle();
     $("i",this).toggleClass("icon-chevron-down icon-chevron-up");
 });
-/*(function(){
-    $('#attendance').load("/?page=2 #attendance-content");
-});
-*/
 function changePage(pageNum){
     $('#attendance').load("/?page=" + pageNum + " #attendance-content");
 };
