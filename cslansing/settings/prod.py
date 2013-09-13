4e01ba53c59c34d68a13c91e69a1abbfdb0a142a
Chris Lansing <cslansin@gmail.com>
2013-09-13 14:02:49 -0400

    left out comma in STATICFILES_DIRS

--- cslansing/settings/prod.py
+++ cslansing/settings/prod.py
@@ -64,7 +64,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    '/home/cslansing/cslansing-com/cslansing/apps/pluggable/static/'
+    '/home/cslansing/cslansing-com/cslansing/apps/pluggable/static/',
 )
 
 # List of finder classes that know how to find static files in
