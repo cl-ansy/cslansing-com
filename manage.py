cf311754883e88892bfacd82d8fbf861461c7f56
Chris Lansing <cslansin@gmail.com>
2013-09-13 13:59:33 -0400

    changed manage.py to use prod settings

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 sys.path.append('/home/cslansing/cslansing-com/cslansing')
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cslansing.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cslansing.settings.prod")
 
     from django.core.management import execute_from_command_line
 
