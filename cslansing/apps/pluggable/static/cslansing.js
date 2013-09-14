04cb50e815a9fc4fb4f5a18d465d09d665e49f5b
Chris Lansing <cslansin@gmail.com>
2013-09-14 12:16:47 -0400

    removed chevron toggle and changed chevron to angle

--- cslansing/apps/pluggable/static/cslansing.js
+++ cslansing/apps/pluggable/static/cslansing.js
@@ -1,6 +1,5 @@
 $('.section-header').click(function() {
     $(this).next('.section-content').slideToggle();
-    $("i",this).toggleClass("icon-chevron-down icon-chevron-up");
     $("html, body").animate({ scrollTop: $(this).offset().top }, 1000);
 });
 function changePage(pageNum){
