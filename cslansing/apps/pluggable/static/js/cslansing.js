fb761a2f0edac1783d38b71c7d5eafcdf6f1e365
Chris Lansing <cslansin@gmail.com>
2013-09-30 13:46:44 -0400

    static restructuring take 3

---
+++ cslansing/apps/pluggable/static/js/cslansing.js
@@ -0,0 +1,7 @@
+$('.section-header').click(function() {
+    $(this).next('.section-content').slideToggle();
+    $("html, body").animate({ scrollTop: $(this).offset().top }, 1000);
+});
+function changePage(targetId, contentId, pageNum){
+    $('#' + targetId).load("/?page=" + pageNum + " #" + contentId);
+};
