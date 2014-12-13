753b07fc1684058804aab540ad9b578c1be1c209
Chris Lansing <cslansin@gmail.com>
2014-12-12 21:46:24 -0500

    hello world of react

--- cslansing/apps/main/scripts/config.js
+++ cslansing/apps/main/scripts/config.js
@@ -1,9 +1,16 @@
 require.config({
     paths: {
-        jquery: 'jquery/dist/jquery'
+        jquery: 'jquery/dist/jquery',
+        react: 'react/react'
     }
 });
 
-define(['jquery'], function ($) {
-    console.log('asdf');
+define(['react'], function (React) {
+    var Hello = React.createClass({displayName: 'Hello',
+        render: function() {
+            return React.createElement("div", null, "Hello ", this.props.name);
+        }
+    });
+
+    React.render(React.createElement(Hello, {name: "World"}), document.body);
 });
